9ccb8ae19987851fe2faa2c5efeda336cb40a54a
blanpiedlab <150059775+blanpiedlab@users.noreply.github.com>
2025-02-19 13:36:06 -0500

    Delete peakAnalysis_ directory

--- peakAnalysis_/getStimList_v2.R
+++
@@ -1,4 +0,0 @@
-#getStimList_v2.R
-source(paste0(path,"peakAnalysis/def_stim_paradigms_v4.R")) 
-
-stimList = def_stim_paradigms(dataframe=df, APcount = num_APs,keys=keys,offset=s88x_offset)
--- peakAnalysis_/get_peak_halfwidths.R
+++
@@ -1,9 +0,0 @@
-#get_peak_halfwidths.R
-
-tic()
-source("Z:\\R Scripts/Sam current as of 7.28.22/Active scripts/pipeline v5 - generalized/peakAnalysis/def_peak_halfwidths_rise_decaytimes_v1.R")
-
-halfWidths = getHalfWidths(df = fitsCleaned, groupers = groupers_plus)
-
-print("Finished getting full-width half-maximums.")
-toc()
