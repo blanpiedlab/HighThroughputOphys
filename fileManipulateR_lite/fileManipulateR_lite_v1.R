ef2b6296539d70d1398f13ec0bd6840df4e8f134
blanpiedlab <150059775+blanpiedlab@users.noreply.github.com>
2025-02-21 15:07:37 -0500

    Add files via upload

---
+++ fileManipulateR_lite/fileManipulateR_lite_v1.R
@@ -0,0 +1,60 @@
+
+#Written Samuel T Barlow 2.21.2025
+#fileManipuleR_lite_v1.R is a function wrapper which can coerce Frame-Intensity files from ImageJ into the preferred format for peakFinder.R.
+#The script performs two functions:
+# 1. the script reads in all the .csv files containing Frame and Intensity data and staples a few expected columns to the data frame. These are: 
+#    fileID - the title of the subdirectory from which the file was read
+#	 timeStamp - a deprecated column which is likely not needed for full functionality. In the original workflow, timeStamp is the date-time format for camera frame times. 
+#	 index - the Frame number, but starting from index 1. 
+# 	 dt - an interpolated frame time based on the known exposure time of the recording. This is a simple linear interpolation in the current format (e.g. frame 1 = 0 s, frame 2 = 0.005 s, etc.)
+
+#This script requires the user to specify a few details.
+# path - the path to the code repository. please specify the outer shell of the directory (e.g. from Github, Drive:\YourPath\HighThroughputOphys-main)
+# dir - the path to your dataset to be processed. This will be the .csv file directory containing the folder "ROIoutputs". 
+# NOTE: The script is expecting to find a directory called "ROIoutputs" so that it can recurse into it for all of the .csv files. Do not specify your dir as a subdirectory below "ROIoutputs". 
+# frameTime - this should be given in seconds.
+
+#The remainder of the global variables need not be manipulated unless the user has adjusted prior steps in the pipeline.
+
+
+
+#list_of_packages(c('plyr','dplyr','gridExtra','zoo','tidyr','reshape2','stringr','readr','purrr','tools','tictoc','sjmisc'))
+#install.packages(list_of_packages)
+
+#Read in usual dependencies
+library(plyr) ###File manipulation
+library(dplyr) ###File manipulation
+library(gridExtra) ###Tabulating data
+library(zoo)  ###not used in this, but mathematical functions
+library(tidyr)
+library(reshape2)
+library(stringr)
+library(readr)
+library(purrr)
+library(tools)
+library(tictoc)
+library(sjmisc)
+
+
+#### USER ADJUSTED PARAMETERS ####
+
+path = "Z:\\R Scripts/Github_repo"
+dir = "Y:\\Sam/test_pipeline/abby_Ca_imaging/abby/test_subset" #data directory
+frameTime = 0.1#time in seconds that each frame is expected to take. This value is used to interpolate the time points for each frame from the frame index. 
+
+
+
+
+
+### PARAMETERS TO TARGET FILES - DO NOT ADJUST UNLESS YOU HAVE MODIFIED OUTPUTS FROM PRIOR IMAGEJ MACROS ###
+
+subfolders = list.dirs(path=dir,full.names=FALSE, recursive=TRUE) 
+subfolders = grep("_Results", subfolders, value=TRUE)
+pattern1 = "_driftcorr-\\d_Results|_driftcorr_Results|-\\d_Results|_Results"
+subfolders_fixed = str_remove(subfolders, pattern = pattern1)
+
+newFilePat = "ROI\\d\\d\\d\\d"
+          
+tic()
+source(paste0(path, "/fileManipulateR/fileRename_lite.R")) ##only run this if you need to rename files still; converts all ROIs to uniform numbering
+toc()
---
+++ fileManipulateR_lite/fileRename_lite.R
@@ -0,0 +1,72 @@
+
+
+
+read_and_write_csv<- function(file,folder, exposureTime){
+                  #path=setwd(folder)
+                  #path=setwd(folder)
+                  #filenames<- list.files(pattern="ROI",full.names=TRUE,recursive=F)
+                  folder_name_fixed<- str_remove(folder, pattern = pattern1)
+                  source_info<-basename(folder)
+                  source_info_fixed<-str_remove(source_info, pattern = pattern1)
+                  print(paste0("This is the new foldername for saving out .csv files: ", folder_name_fixed))
+          
+                  newFolder <- paste0(folder_name_fixed,"_stamped/")  #create the save folder for these stamped ROIs
+                  dir.create(newFolder)
+                  
+                  print(paste0("This is the source info ", source_info))
+                  
+                  tmp_file<- read.csv(file = file,header=TRUE) 
+                  colnames(tmp_file)[1]<- "Frame"
+                  colnames(tmp_file)[2]<- "Intensity"
+                  tmp_file$fileID<- source_info_fixed
+                  tmp_file$index <-seq.int(nrow(tmp_file))
+                  tmp_file$timeStamps<- "placeholder"
+                  tmp_file$dt <- tmp_file$index*exposureTime - exposureTime
+                  #print(head(tmp_file))
+                  
+                  newfilename<- paste0(newFolder,str_extract(file, pattern=newFilePat),"stamped.csv")
+                  
+                  write.csv(x = tmp_file, file = newfilename,row.names=FALSE)
+                                           #read in the stampfile that we've kept
+                  
+}
+
+renameFunc<- function(folder) {
+              path=setwd(folder)
+              path=setwd(folder) ##Calling this twice in a row fixes a weird bug where it lags the folder rename by 1 because it's calling the primary_folder as the initial subdirectory
+              
+              filenames<- list.files(pattern="ROI", full.names=TRUE,  recursive=F)
+              details = file.info(filenames)
+              details = details[with(details, order(as.POSIXct(mtime))),] #This line orders the files by the time they were created (ROI indexing)
+              files = rownames(details)
+
+              placeholder = basename(path)
+            
+              b<- sprintf("%s_ROI%04d.csv", placeholder,seq(files)) #Here, sprintf is adding padding zeros to normalize the data read
+              file.rename(files,b)
+
+  
+}
+
+
+
+
+primary_folder = dir
+subfolders = grep(pattern="_Results",list.dirs(primary_folder,  full.names = T, recursive = T), value = T)
+
+
+lapply(subfolders, renameFunc)
+
+
+
+for (i in subfolders){
+      tmp_subfolder<- i
+      setwd(tmp_subfolder)
+      setwd(tmp_subfolder)
+      fnames <- list.files(pattern="ROI", full.names=TRUE,recursive=FALSE)      
+      
+      lapply(fnames, read_and_write_csv,folder = tmp_subfolder, exposureTime = frameTime)
+      
+}
+
+    
